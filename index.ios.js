b60b901602bb164c33d3e6d12ef726a81812b8fc
James Lu <jlptf0420@gmail.com>
2017-03-10 15:05:49 +0800

    create project for react-native iOSTabBar sample

---
+++ index.ios.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export default class iOSTabBar extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Icon name="ios-person" size={30} color="#4F8EF7" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('iOSTabBar', () => iOSTabBar);
